a6884caaf4a4b4d755663189d599bfe9b0573eb9
Sparsh Verma <sparshvermadmc@gmail.com>
2021-01-30 21:44:01 +0530

    changes for ID property

--- pet-clinic-data/src/main/java/verma/sparsh/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/verma/sparsh/petclinic/services/map/PetServiceMap.java
@@ -19,7 +19,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
 
     @Override
     public Pet save(Pet object) {
-        return save(object.getId(),object);
+        return save(object);
     }
 
     @Override
