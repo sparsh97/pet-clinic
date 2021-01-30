2c7faf7c841834d2e492182d1a225c900cde808d
Sparsh Verma <sparshvermadmc@gmail.com>
2021-01-30 21:24:10 +0530

    Displaying Vets Data on index page closes #12

--- pet-clinic-web/src/main/java/verma/sparsh/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/verma/sparsh/petclinic/controllers/VetController.java
@@ -1,14 +1,21 @@
 package verma.sparsh.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import verma.sparsh.petclinic.services.VetService;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
