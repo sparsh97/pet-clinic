62e195c0847ed4daa193128b556a004f1c182ff3
Sparsh Verma <sparshvermadmc@gmail.com>
2021-02-06 22:02:39 +0530

    Added pet and petType for owner. Command Line runner error solved.

--- pet-clinic-data/src/main/java/verma/sparsh/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/verma/sparsh/petclinic/services/map/OwnerServiceMap.java
@@ -32,24 +32,26 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-
-        if (object != null) {
+        if(object != null){
             if (object.getPets() != null) {
                 object.getPets().forEach(pet -> {
-                    if (pet.getPetType() != null) {
-                        if (pet.getPetType().getId() == null) {
+                    if (pet.getPetType() != null){
+                        if(pet.getPetType().getId() == null){
                             pet.setPetType(petTypeService.save(pet.getPetType()));
-                        } else {
-                            throw new RuntimeException("PetType is required....");
                         }
-                        if (pet.getId() == null) {
-                            Pet savePet = petService.save(pet);
-                            pet.setId(savePet.getId());
-                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if(pet.getId() == null){
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
                     }
                 });
             }
+
             return super.save(object);
+
         } else {
             return null;
         }
