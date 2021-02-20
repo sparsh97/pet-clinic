5111b2f80a818078c6dc5ba2c8152028c6c970a9
Sparsh Verma <sparshvermadmc@gmail.com>
2021-02-20 12:52:14 +0530

    Implemented Visit Map Service. closes #29

--- pet-clinic-data/src/main/java/verma/sparsh/petclinic/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/verma/sparsh/petclinic/services/map/VisitServiceMap.java
@@ -20,6 +20,10 @@ public class VisitServiceMap extends AbstractMapService<Visit, Long> implements 
 
     @Override
     public Visit save(Visit object) {
+        if(object.getPet() == null || object.getPet().getOwner()== null
+                || object.getPet().getId()== null || object.getPet().getOwner().getId()== null){
+            throw new RuntimeException("Invalid Visit");
+        }
         return super.save(object);
     }
 
