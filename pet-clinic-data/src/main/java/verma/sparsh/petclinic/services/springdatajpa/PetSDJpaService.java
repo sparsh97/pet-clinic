4720b75988ece0b3570dec3bad8c8c59295d1b84
Sparsh Verma <sparshvermadmc@gmail.com>
2021-02-20 12:12:16 +0530

    Implemeted Spring Data JPA Service for Vet. closes #24

---
+++ pet-clinic-data/src/main/java/verma/sparsh/petclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,46 @@
+package verma.sparsh.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import verma.sparsh.petclinic.model.Pet;
+import verma.sparsh.petclinic.repositories.PetRepository;
+import verma.sparsh.petclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petSet=new HashSet<>();
+        petRepository.findAll().forEach(petSet::add);
+        return petSet;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
