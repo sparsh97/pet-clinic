43e675f503e09cfb4ed2e9eb6c655b8bd7dfae23
Sparsh Verma <sparshvermadmc@gmail.com>
2021-03-30 23:10:49 +0530

    display owner done

--- pet-clinic-web/src/main/java/verma/sparsh/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/verma/sparsh/petclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package verma.sparsh.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import verma.sparsh.petclinic.services.OwnerService;
 
 @Controller
@@ -21,7 +24,13 @@ public class OwnerController {
 
     @RequestMapping("/owners/find")
     public String findOwners(){
-
         return "notimplemented";
+    }
+
+    @GetMapping("/owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
